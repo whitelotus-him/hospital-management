892079cf5a0a31d87d96e99427e816c8d5ae146f
whitelotus-him <whitelotus.him@gmail.com>
2025-11-15 21:05:01 +0530

    Day 2: Create forms.py with LoginForm and PatientRegisterForm
    Signed-off-by: whitelotus-him <whitelotus.him@gmail.com>

---
+++ app/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField, DateField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from app.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class PatientRegisterForm(FlaskForm):
+    name = StringField('Full Name', validators=[DataRequired(), Length(min=2, max=100)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone', validators=[DataRequired(), Length(min=10, max=15)])
+    date_of_birth = DateField('Date of Birth', validators=[DataRequired()])
+    address = TextAreaField('Address', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', 
+                                     validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
+    submit = SubmitField('Register')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already registered. Please use a different email.')
