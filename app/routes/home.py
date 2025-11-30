e2d050b0c1f14f7e5c1d0f6a9e490a71a0a9dabb
whitelotus-him <whitelotus.him@gmail.com>
2025-11-30 10:29:46 +0530

    Fix home.py to use Blueprint pattern for consistency with other routes
    Signed-off-by: whitelotus-him <whitelotus.him@gmail.com>

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,6 +1,7 @@
-from app import app
-from flask import render_template
+from flask import Blueprint, render_template
 
-@app.route('/')
+bp = Blueprint('home', __name__)
+
+@bp.route('/')
 def home():
     return render_template('home.html')
